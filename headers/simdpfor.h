60b821f071e5de126127727f5a6b982b1336b8c1
Omar Tanner <pixzlert@gmail.com>
2025-12-26 22:43:32 +0000

    try: don't store, unpack individual exception gaps in extra pass (0 locality prefetch)

--- headers/simdpfor.h
+++ headers/simdpfor.h
@@ -353,7 +353,7 @@ public:
       const size_t next_elem = next_exception >> 2;
       const uint64_t next_bitpos = uint64_t(next_elem) * b;
       const size_t next_word = (next_bitpos >> 5) * 4 + next_lane;
-      __builtin_prefetch(&start[next_word], 0, 1);
+      __builtin_prefetch(&start[next_word], 0, 0);
 
       *delta_sum += (-gap + (*i));
       i++;
